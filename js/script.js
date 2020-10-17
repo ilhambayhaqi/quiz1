29453b7beaab6c3d350679d377e6fb1eefa9df47
ilhambayhaqi <ilham.bhq28@gmail.com>
2020-10-17 20:16:15 +0700

    add a jquery script

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(window).on("load",function() {
+  $(window).scroll(function() {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".front").each(function() {
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      
+      if (objectBottom < windowBottom) {
+        if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
+      } else {
+        if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
+      }
+    });
+  }).scroll();
+});
